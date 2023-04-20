5b155ca5a746577df994fec0fa5af61e34bcfa3f
Radwa Elgammal <elgammalr74@gmail.com>
2023-04-20 14:28:29 +0200

    .

--- Models/categoryModel.js
+++ Models/categoryModel.js
@@ -1,16 +1,31 @@
-const mongoose = require ('mongoose');
+// const mongoose = require ('mongoose');
 
-const AutoIncreament = require ('mongoose-sequence')(mongoose);
+// const AutoIncreament = require ('mongoose-sequence')(mongoose);
+
+// const increment=require('@sudtanj/typegoose-auto-increment')
+// const categorySchema = new mongoose.Schema({
+//   _id:{type:Number},
+//   catName: { type: String, required: true },
+//   description: { type: String},
+// }
+// );
+
+//  categorySchema.plugin(AutoIncreament,{id:'categoryCounter'});
+
+// mongoose.model("Category",categorySchema);
+
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const categorySchema = new mongoose.Schema({
-  _id:{type:Number},
+  _id: { type: Number },
   catName: { type: String, required: true },
-  description: { type: String},
-}
-);
+  description: { type: String },
+},{_id:false});
 
- categorySchema.plugin(AutoIncreament,{id:'categoryCounter'});
+// categorySchema.plugin(AutoIncrement, { id: 'categoryCounter' });
+categorySchema.plugin(AutoIncrement, { inc_field: '_id' ,id:'categoryCounter'});
 
-mongoose.model("Category",categorySchema);
 
+mongoose.model('Category', categorySchema);
 
